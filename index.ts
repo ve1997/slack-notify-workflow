b31c593369b8f49cd31ce553bb82fff8be22f9e6
KazuyaSuzuki <83203852+ve1997@users.noreply.github.com>
2024-11-05 16:18:48 +0900

    feat

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import figlet from "figlet";
+
+const server = Bun.serve({
+  port: 3000,
+  fetch(req) {
+    const body = figlet.textSync("Bun!");
+    return new Response(body);
+  },
+});
+
+console.log(`Listening on http://localhost:${server.port} ...`);
